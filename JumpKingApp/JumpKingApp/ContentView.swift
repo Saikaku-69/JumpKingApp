54b63dcf7d003e377a53709fa737e88caeccf881
Saikaku-69 <grnmnsta@gmail.com>
2024-08-13 00:19:46 +0900

    main
    festivalAppSample_1

---
+++ JumpKingApp/JumpKingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JumpKingApp
+//
+//  Created by cmStudent on 2024/08/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JumpKingApp/JumpKingApp/JumpKingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JumpKingAppApp.swift
+//  JumpKingApp
+//
+//  Created by cmStudent on 2024/08/13.
+//
+
+import SwiftUI
+
+@main
+struct JumpKingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
