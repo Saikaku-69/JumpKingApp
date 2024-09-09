ccc50898be1ca8a8b36c34857773f6507b75fc24
Saikaku <grnmnsta@gmail.com>
2024-09-09 15:08:46 +0900

    test

---
+++ JumpKingApp/JumpKingApp/FrontView/MenuView.swift
@@ -0,0 +1,32 @@
+////
+////  MenuView.swift
+////  JumpKingApp
+////
+////  Created by cmStudent on 2024/09/09.
+////
+//
+//import SwiftUI
+//
+//struct ItemList: Identifiable {
+//    let id = UUID()
+//    let icon: String
+//    let name: String
+//}
+//
+//let itemList = [
+//    ItemList(icon: "abokado", name: "アボカド"),
+//    ItemList(icon: "starItem", name: "無敵")
+//]
+//
+//struct MenuView: View {
+//    var body: some View {
+//       //
+//        ForEach (itemList) { item in
+//            Image(item)
+//        }
+//    }
+//}
+//
+//#Preview {
+//    MenuView()
+//}
