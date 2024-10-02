05c07843faba2220453122069095357ee508a374
Saikaku <grnmnsta@gmail.com>
2024-10-02 19:22:31 +0900

    created demo

---
+++ JumpKingApp/JumpKingApp/JieYuanBao/ChooseMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  ChooseMenuView.swift
+//  JumpKingApp
+//
+//  Created by cmStudent on 2024/10/02.
+//
+
+import SwiftUI
+
+struct ChooseMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChooseMenuView()
+}
